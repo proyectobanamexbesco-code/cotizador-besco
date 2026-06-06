e2ebfc81b4fc5e153614b440c0c0f31f7c8f49eb
Aplicaciones GM Reportes <proyectobanamexbesco@gmail.com>
2026-06-06 00:04:12 -0600

    Update cotizador.py

--- cotizador.py
+++ cotizador.py
@@ -1,1 +1,4 @@
-
+import streamlit as st
+st.set_page_config(page_title="Sistema Grupo Besco", layout="wide")
+st.title("🏗️ Panel de Soluciones Grupo Besco")
+st.markdown("Utiliza el menú lateral izquierdo para acceder a las herramientas.")
